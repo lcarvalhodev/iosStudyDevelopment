d6c6dabb85cd4553133f0459981c753c89cf677f
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-01-02 22:17:04 -0300

    Initial Commit

---
+++ studyDevelopmentProject/studyDevelopmentProject.h
@@ -0,0 +1,18 @@
+//
+//  studyDevelopmentProject.h
+//  studyDevelopmentProject
+//
+//  Created by Leandro Almeida de Carvalho on 02/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for studyDevelopmentProject.
+FOUNDATION_EXPORT double studyDevelopmentProjectVersionNumber;
+
+//! Project version string for studyDevelopmentProject.
+FOUNDATION_EXPORT const unsigned char studyDevelopmentProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <studyDevelopmentProject/PublicHeader.h>
+
+
