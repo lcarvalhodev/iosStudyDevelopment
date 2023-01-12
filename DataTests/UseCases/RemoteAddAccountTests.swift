0aa91f136e70ae5f026c90437c3c82f1b702d9a7
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-01-12 19:38:55 -0300

    add memoryLeak check

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -46,7 +46,15 @@ extension RemoteAddAccountTests {
     func makeSut(url: URL = URL(string: "http://any-url.com")!) -> (sut: RemoteAddAccount, HttpClientSpy: HttpClientSpy) {
         let httpClientSpy = HttpClientSpy()
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        checkMemoryLeak(for: sut)
+        checkMemoryLeak(for: httpClientSpy)
         return (sut, httpClientSpy)
+    }
+    
+    func checkMemoryLeak (for instance: AnyObject, file: StaticString = #file, line: UInt = #line){
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, file: file, line: line)
+        }
     }
     
     func expect (_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void, file: StaticString = #file, line: UInt = #line){
