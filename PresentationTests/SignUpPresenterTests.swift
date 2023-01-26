3751d4f1f931e47e2b3c8b7eee925cc716956f50
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-01-25 21:03:55 -0300

    ensure SignUpPresenter shows error message if no password is provided

--- PresentationTests/SignUpPresenterTests.swift
+++ PresentationTests/SignUpPresenterTests.swift
@@ -15,6 +15,10 @@ class SignUpPresenter {
         else if(viewModel.email == nil || viewModel.email!.isEmpty){
             alertView.showMessage(viewModel: AlertViewModel(title: "Validation fails", message: "Field email should be provided"))
         }
+        
+        else if(viewModel.password == nil || viewModel.password!.isEmpty){
+            alertView.showMessage(viewModel: AlertViewModel(title: "Validation fails", message: "Field password should be provided"))
+        }
     }
 }
 
@@ -48,6 +52,13 @@ final class SignUpPresenterTests: XCTestCase {
         let signUpViewModel = SignUpViewModel(name: "any_name", password: "any_password", passwordConfirmation: "any_password")
         sut.signUp(viewModel: signUpViewModel)
         XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Validation fails", message: "Field email should be provided"))
+    }
+    
+    func test_signUp_should_show_error_message_if_password_is_not_provided() throws {
+        let (sut, alertViewSpy) = makeSut()
+        let signUpViewModel = SignUpViewModel(name: "any_name", email: "any_email", passwordConfirmation: "any_password")
+        sut.signUp(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Validation fails", message: "Field password should be provided"))
     }
 }
 
