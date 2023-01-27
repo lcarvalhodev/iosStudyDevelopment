800e7afc20e713886eec9587cbdf27a15615f6d8
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-01-27 19:36:52 -0300

    ensure SignUpPresenter calls emailValidator with correct email

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -3,8 +3,10 @@ import Foundation
 public final class SignUpPresenter {
     
     private let alertView: AlertView
+    private let emailValidator: EmailValidator
     
-    public init(alertView: AlertView) {
+    public init(alertView: AlertView, emailValidator: EmailValidator) {
+        self.emailValidator = emailValidator
         self.alertView = alertView
     }
     
@@ -18,21 +20,19 @@ public final class SignUpPresenter {
         if(viewModel.name == nil || viewModel.name!.isEmpty){
            return "Field name should be provided"
         }
-        
         else if(viewModel.email == nil || viewModel.email!.isEmpty){
             return "Field email should be provided"
         }
-        
         else if(viewModel.password == nil || viewModel.password!.isEmpty){
             return "Field password should be provided"
         }
-        
         else if(viewModel.passwordConfirmation == nil || viewModel.passwordConfirmation!.isEmpty){
             return "Field password confirmation should be provided"
         }
         else if(viewModel.password != viewModel.passwordConfirmation){
             return "Field password confirmation should be equal to field password"
         }
+        _ = emailValidator.isValid(email: viewModel.email!)
         return nil
     }
 }
