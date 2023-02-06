5ada7e0bdfd2957db8514ac9c03cb6fc1256a65d
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-02-06 19:24:36 -0300

    move sut creation to a factory method

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -6,21 +6,23 @@ import Presentation
 final class SignUpViewControllerTests: XCTestCase {
 
     func test_leading_is_hidden_on_start() throws {
-        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        sut.loadViewIfNeeded ()
-        XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
+        XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
     }
     
     func test_sut_is_implements_loadingView() throws {
-        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        XCTAssertNotNil(sut as LoadingView)
+        XCTAssertNotNil(makeSut() as LoadingView)
     }
     
     func test_sut_is_implements_alertView() throws {
+        XCTAssertNotNil(makeSut() as AlertView )
+    }
+}
+
+extension SignUpViewControllerTests {
+    func makeSut() -> SignUpViewController {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(identifier: "SignUpViewController") as! SignUpViewController
-        XCTAssertNotNil(sut as AlertView )
+        sut.loadViewIfNeeded ()
+        return sut
     }
 }
