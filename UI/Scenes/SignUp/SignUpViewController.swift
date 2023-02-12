59c68c761629e915e697edd134c5d06658cbbc3e
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-02-12 01:53:18 -0300

    add SignUpViewController factory

--- UI/Scenes/SignUp/SignUpViewController.swift
+++ UI/Scenes/SignUp/SignUpViewController.swift
@@ -2,17 +2,17 @@ import Foundation
 import UIKit
 import Presentation
 
-final class SignUpViewController: UIViewController, Storyboarded {
+public final class SignUpViewController: UIViewController, Storyboarded {
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var  emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmationTextField: UITextField!
     
-    var signUp: ((SignUpViewModel) -> Void)?
+    public var signUp: ((SignUpViewModel) -> Void)?
     
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
@@ -30,7 +30,7 @@ final class SignUpViewController: UIViewController, Storyboarded {
 }
 
 extension SignUpViewController: LoadingView{
-    func display(viewModel: Presentation.LoadingViewModel) {
+    public func display(viewModel: Presentation.LoadingViewModel) {
         if(viewModel.isLoading){
             view.isUserInteractionEnabled = false
             loadingIndicator?.startAnimating()
@@ -42,7 +42,7 @@ extension SignUpViewController: LoadingView{
 }
 
 extension SignUpViewController: AlertView{
-    func showMessage(viewModel: Presentation.AlertViewModel) {
+    public func showMessage(viewModel: Presentation.AlertViewModel) {
         let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
         present(alert, animated: true)
