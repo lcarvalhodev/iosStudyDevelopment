66e408507683a00c91d7c36eeee3ffe6f787a8ac
Leandro Carvalho <lcarvalho.dev@gmail.com>
2023-02-19 15:51:14 -0300

    e2e test to guarantee that the same cannot be inserted twice

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -9,7 +9,7 @@ final class AddAccountIntegrationTests: XCTestCase {
         let alamofireAdapter = AlamofireAdapter()
         let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url , httpClient: alamofireAdapter)
-        let addAccountModel = AddAccountModel(name: "Test Swift", email: "test2Swift@swift.com", password: "swift123", confirmPassword: "swift123")
+        let addAccountModel = AddAccountModel(name: "Test Swift", email: "\(UUID().uuidString)@swift.com", password: "swift123", confirmPassword: "swift123")
         let exp = expectation(description: "waiting")
         sut.add(addAccountModel: addAccountModel){ result in
             switch(result) {
@@ -20,5 +20,16 @@ final class AddAccountIntegrationTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 10)
+        let exp2 = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel){ result in
+            switch result {
+            case .failure(let error) where error == .emailInUse:
+                 XCTAssertNotNil(error)
+            default:
+                XCTFail("Expect failure got \(result) instead")
+            }
+            exp2.fulfill()
+        }
+        wait(for: [exp2], timeout: 10)
     }
 }
